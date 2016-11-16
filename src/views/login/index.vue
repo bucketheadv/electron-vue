fd825f63c0640ff81f90dca71fe87723e05662ad
sven <sven199109@gmail.com>
2016-11-16 12:11:59 +0800

    login page modify

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,16 +2,16 @@
   <div>
     <div class="loginForm">
       <img :src="logo" class="logoImg" />
-      <form class="login" v-on:submit.prevent="submit">
-        <div class="line">
+      <form class="ui form login" v-on:submit.prevent="submit">
+        <div class="line field">
           <div v-show="btn && !form.id">id不能为空</div>
           <input type="number" placeholder="输入你的id" v-model="form.id">
         </div>
-        <div class="line">
+        <div class="line field">
           <div v-show="btn && !form.name">用户名不能为空</div>
           <input type="text" placeholder="输入你的用户名" v-model="form.name">
         </div>
-        <button>登录</button>
+        <button class="ui button">登录</button>
       </form>
     </div>
   </div>
